735ff4e6aceb2f0b3325c226fe9d196f70b6d911
Praisg <120684474+Praisg@users.noreply.github.com>
2023-07-31 04:58:37 +0200

    my fix code challenge

---
+++ 0x00-challenge/2-sort.rb
@@ -0,0 +1,31 @@
+###
+#
+#  Sort integer arguments in scending order
+#
+###
+
+result = []
+ARGV.each do |arg|
+    # skip if ! int
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to int
+    i_arg = arg.to_i
+
+    # insert result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
+        end
+    end
+    result << i_arg if !is_inserted
+end
+
+puts result
